4444ac70e90da96401149c95785d2e556ebbb051
Lindina06 <160626607+Lindina06@users.noreply.github.com>
2026-04-07 08:43:40 +0200

    Add files via upload

---
+++ lezione_1.py
@@ -0,0 +1,117 @@
+# Questo è un commento
+# ctrl + ù -> attiva/disattiva commento
+# Python non è un linguaggio fortemente tipizzato
+# String miaVar -> Java -> fortemente tipizzato 
+# print vuol dire stampa e lo fa con la variabile che sta sopra 
+# per farlo per più variabili devi ripetere sotto ad ogni assegnazione della variabile il comando print
+# type insieme a print ti indica il tipo di dato (nel terminale) 
+
+miaVar = 5
+#print(miaVar, type(miaVar))
+miaVar = "testo"
+#print(miaVar, type(miaVar))
+miaVar = True 
+#print(miaVar, type(miaVar))
+
+abc = None
+#print(abc)
+
+# Nomenclatura di un elemento python
+nomeDatoVariabile = 0   # CamelCase -> ad ogni parola l'inizio è con la lettera maiuscola
+NomeDatoVariabile = 0   #PascalCase -> anche la prima lettera maiuscola
+nome_dato_variabile = 0 #SnakeCase -> con l'underscore e non con il trattino 
+NOME_DATO_VARIABILE = 0 # con tutto maiuscolo di solito definiscono le costanti -> variabili con contenuto fisso (non costante)
+
+# Assegnazioni singole
+x = 1
+y = 2
+z = 3
+
+# Assegnazioni multiple
+x,y,z = 1,2,3
+x = y = z = 1 #Assegnare lo stesso valore alle variabili
+
+# Tipi di dato
+# - int     -> numerico intero   -> d = 5
+# - float   -> numerico decimale -> d = 5,5 
+# - string  -> testo             -> d = "testo"
+# - boolean -> True/False        -> d = True
+# - none    -> None              -> d= None
+
+# Funzioni predefinite in Python 
+# print(args)    -> Funzione predefinita che stampa nel terminale il valore di args
+# type(var)      -> Funzione predefinita che restituisce il tipo di dato di una variabile 
+# input()        -> Funzione predefinita che permette l'inserimento di valori da terminale
+# len(str)       -> Funzione predefinita che restituisce la lunghezza di una stringa
+# help(func)     -> Restuisce la documentazione di una funzione passata come un parametro 
+
+# Funzioni predefinite di Casting
+# int(param)       -> Restituisce il valore inserito in formato int
+# float(param)     -> Restituisce il valore inserito in formato float
+# str(param)       -> Restituisce il valore inserito in formato str
+# bool(param)      -> Restituisce il valore inserito in formato bool
+# bool(0)          ->striga/contenitore/valore/lista vuota è False se c'è qualcosa dentro è True
+
+miaVar = "sono una Stringa"
+print(miaVar)
+print(type(miaVar))
+print(len(miaVar))
+nome = 'Mario Rossi' #input("Inserisci un nome: ")
+print("il nome che hai inserito è: ",nome)
+age = '25' #input("Inserisci età: ") # -> stringa
+eta = int(age) # trasformo la stringa in un valore intero
+#eta = float(age) # trasformo la stringa in un valore decimale
+#eta = bool(age) # trasformo la stringa in un valore booleano
+print(type(eta))
+
+print("FINE")
+
+# String in python
+# Per creare una stringa in python si utilizzano '' o ""
+# Per creare una stringa multiriga si utilizzano ''' ''' oppure """ """
+# Possiamo accedere ai singoli caratteri di una stringa tramite l'indice [i]
+#Possiamo selezionare porzioni di stringhe[n:m]
+# indexing sfruttare un indice per ricavare un valore
+# slicing taglia la stringa e prende sottoinsieme
+# Metodi predefiniti per manipolare una stringa
+
+multiStr = """ Sono
+           una
+            stringa"""
+    #  0123456789......
+str = " Sono una Stringa " # stringa a singola riga
+print(str[0]) 
+print(str[5:8]) # non include m in [n:m] 
+print(str[:8]) # restituirà dall'inizio della stringa
+print(str[5:]) # restituirà dalla fine della stringa
+print(str[-5:-1]) # inizierà a contare dalla fine quindi dalla r fino a g
+print(str[-7:]) # partirà da s e poi prenderà in considerazione la fine da s in poi 
+
+print(str.lower()) # Restituisce una copia di una stringa in minuscolo
+print(str.upper()) # Restituisce una copia di una stringa in maiuscolo
+print(str.strip()) # Restituisce una copia di una stringa senza gli spazi all'inizio e alla fine
+print(str.replace('n','x', 1)) # Restituisce una copia di una stringa tipo 'n'(Ielemento),(con)'x', 1 ->(solo del primo elemento che incotri nella stringa)
+print(str.split(' ')) # Restituisce una contenitore con porzioni di stringa tagliate in base ad un separatore
+print(str)
+
+nome= "Mario"
+cognome= "Rossi"
+eta = 25
+# saluta = "Ciao " + nome + " " + cognome + "anni " + "sono una stringa!"
+saluta = "Ciao {} {} anni {} sono una stringa!" # {} sono segnaposti che specifici dentro cosa va con format(nomeV1,nomeV2 etc)
+print(saluta.format(nome, cognome, eta))
+saluta =  "Ciao {1} {0} anni {2} sono una stringa!"
+print(saluta.format(cognome, nome, eta))
+#                       0     1    2
+saluta =  "Ciao {name} {lastname} anni {age} sono una stringa!"
+print(saluta.format(name = nome, lastname = cognome, age = eta))
+
+print(f"Ciao {nome} {cognome} anni {eta} sono una stringa!")
+
+
+age =15
+if age >= 18:
+    print("Maggiore di 18")
+else:
+    print("Minore o uguale di 18")
+    
---
+++ lezione_2.py
@@ -0,0 +1,98 @@
+#https://github.com/emanueleumberto/its_agnesi_data4
+# Operatori
+# - Operatori di assegnamento [=]
+# - Operatori aritmetici [+ - * / %(il modulo: da il resto) **(elevamento a potenza) //(divisione tra interi che da numero intero)]
+# - Operatori aritmetici [+= (aggiungi e assegna) -= *= /= %=]
+# - Operatori di comparazione [>, <, ==, !=(not: cioè diverso da), >=, <=, is, is not, in, not in] -> booleani: True or False
+# - Operatori logici [and or not (nrgo una condizione) xor(condiziona vera se entrambe sono false)]
+
+# AND
+# TRUE and TRUE -> TRUE
+# TRUE and FALSE -> FALSE
+# FALSE and TRUE -> FALSE
+# FALSE and FALSE -> FALSE
+
+# OR
+# TRUE or TRUE -> TRUE
+# TRUE or FALSE -> TRUE
+# FALSE or TRUE -> TRUE
+# FALSE or FALSE -> FALSE
+
+# XOR
+# TRUE or TRUE -> FALSE
+# TRUE or FALSE -> FALSE
+# FALSE or TRUE -> FALSE
+# FALSE or FALSE -> TRUE
+
+# NOT
+# NOT TRUE -> FALSE
+# NOT FALSE -> TRUE
+
+a = 10
+b = 3
+c = 3
+
+print(int(a/b))
+print(a//b)
+print(a%b)
+print(3**3)
+
+#a = a + 5
+a += 5
+print(a>b)
+print(a == b)
+print(a != b)
+print('i' in 'Ciao')
+print('x' not in 'Ciao')
+
+a = 10
+b = 10
+c = 3
+print( a > b and b > c)
+print( a > b or b > c)
+print(not(a < b))
+
+# Strutture di controllo
+# - IF ELIF(else if) ELSE
+#   if condizione: 
+#       blocco di istruzioni
+#   elif condizione:
+#       blocco di istruzioni
+#   else:
+#       blocco di istruzioni 
+#
+# - if condizione: istruzioni else: istruzioni
+
+if a > b:
+    print("A è maggiore di B")
+elif a == b:
+   print("A è uguale a B")
+else:
+    print("A è minore di B")
+
+
+# Strutture iterative 
+# - WHILE
+#   while condizione:
+#       blocco di istruzioni
+num = 0
+while num < 5:
+    print("blocco d'istruzioni")
+    num += 1
+    
+while True: #finchè vero è vero cicla
+    scelta = input("Inserisci il tuo nome oppure fine per terminare: ") 
+    if scelta == "fine": 
+        break #per fare terminare il ciclo tramite l'inserimento fine
+    
+    print(f"Ciao {scelta}")
+    
+print("FINE")
+
+# - FOR
+#   for ele in iterable:
+#        blocco di istruzioni 
+    
+    
+    
+    
